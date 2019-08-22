bccc142ae687df84dfbb62e0131686bbccd9c0e2
Jorge Conde <jorge@referralsaasquatch.com>
2019-08-22 13:50:49 -0700

    Updates

--- src/jwt.js
+++ src/jwt.js
@@ -1,5 +1,8 @@
 import jwt from "jsonwebtoken";
 
+// THIS IS NOT PRODUCTION READY CODE
+// Generating the JWT token should be done in the backend
+
 const getToken = (user, key) => {
   const token = jwt.sign(
     {
